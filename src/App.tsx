67cdbd97de3c2e13c984d9bac4bb334a2934c75e
tradingforex777777-boop <tradingforex777777@gmail.com>
2026-04-21 01:17:39 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -530,7 +530,7 @@ const Footer = () => (
           </a>
           <a href="#" className="w-10 h-10 rounded-sm border border-coffee-tan/20 flex items-center justify-center hover:bg-coffee-tan hover:text-black transition-all duration-500">
             <img 
-              src="https://iili.io/BSHHbMx.png" 
+              src="https://iili.io/BrnPXrx.png" 
               alt="icon tiktok" 
               className="h-10 w-auto object-contain"
               referrerPolicy="no-referrer"
